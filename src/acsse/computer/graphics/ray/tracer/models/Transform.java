0275be52df39000dfff80266369a524f6e573796
SMGuellord <muhiyasumba@gmail.com>
2019-03-20 14:22:03 +0200

    Implemented Basic Transformations.

--- src/acsse/computer/graphics/ray/tracer/models/Transform.java
+++ src/acsse/computer/graphics/ray/tracer/models/Transform.java
@@ -2,9 +2,36 @@ package acsse.computer.graphics.ray.tracer.models;
 
 public class Transform {
 
-    public Transform (){
+    public static Vector rotateAboutX(float angle, Vector vec){
 
+        float sin = (float)Math.sin(angle);
+        float cos = (float)Math.cos(angle);
+        float Y = vec.getY()* cos + vec.getZ() * (-sin);
+        float Z = vec.getY()* sin + vec.getZ() * cos;
+
+        return new Vector(vec.getX(), Y, Z);
     }
 
+    public static Vector rotateAboutY(float angle, Vector vec){
 
+        float sin = (float)Math.sin(angle);
+        float cos = (float)Math.cos(angle);
+        float X = vec.getX()* cos + vec.getY() * sin;
+        float Z = vec.getX()* (-sin) + vec.getZ() * cos;
+
+        return new Vector(X, vec.getY(), Z);
+    }
+
+    public static Vector rotateAboutZ(float angle, Vector vec){
+        float sin = (float)Math.sin(angle);
+        float cos = (float)Math.cos(angle);
+        float X = vec.getX()* cos + vec.getY() * (-sin);
+        float Y = vec.getX()* sin + vec.getY() * cos;
+
+        return new Vector(X, Y, vec.getZ());
+    }
+
+    public Vector scale (float scalar, Vector vec){
+        return new Vector(scalar * vec.getX(), scalar * vec.getY(), scalar * vec.getZ() );
+    }
 }
